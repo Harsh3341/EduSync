9325c764eeb92e98a4d819f53153a6f79c99e25f
Harsh Gupta <harshgupta3341@gmail.com>
2024-01-29 13:19:49 +0530

    added database

---
+++ app.ts
@@ -0,0 +1,25 @@
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.get("/", (_req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello");
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
