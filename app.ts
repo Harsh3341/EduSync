115a5265791f0496aeb087eafcca53e1c16a665e
Harsh Gupta <harshgupta3341@gmail.com>
2024-01-29 13:49:12 +0530

    added basic middleware

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import ErrorMiddleware from "./middleware/error";
 
 app.use(express.json({ limit: "50mb" }));
 
@@ -23,3 +24,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+app.use(ErrorMiddleware);
