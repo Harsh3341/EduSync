4bae63299587bb85bdb8da75d809c6c5ce90210b
Harsh Gupta <harshgupta3341@gmail.com>
2024-08-18 15:49:33 +0530

    initial project setup

---
+++ apps/server/app.ts
@@ -0,0 +1,31 @@
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Hello World",
+  });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(ErrorMiddleware);
---
+++ apps/server/middleware/error.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+
+export const ErrorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "jsonWebTokenError") {
+    const message = "Invalid token. Please login again";
+    err = new ErrorHandler(message, 400);
+  }
+
+  if (err.name === "TokenExpiredError") {
+    const message = "Token expired. Please login again";
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
